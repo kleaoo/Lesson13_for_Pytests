9d26ea2bd230e29c8b958011c11738f511b2a451
USER-PC9 <andre11ytikhonov4627@gmail.com>
2023-12-22 17:53:17 +0300

    Код с урока 13, автотесты

---
+++ test_functions.py
@@ -0,0 +1,12 @@
+from functions import salary,hello_who
+
+#def test_hello_who_max():
+    #assert hello_who('Max') == 'Hello,Max'
+    #assert hello_who('Maxim') == 'Hello,Maxim'
+    #assert hello_who('Maximka') == 'Hello,Maximka'
+    #assert hello_who('Maxf') == 'Hello,Maxf'
+    #assert hello_who('Nikita') == 'Hello,Nikita'
+
+def test_salary():
+    assert(2, 2) != 8
+    assert(3, 1) != 6
